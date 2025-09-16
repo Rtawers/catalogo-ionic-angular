574c7b01f96661b893663614f18882f97e0993bc
Daniel R <rtawers2000@gmail.com>
2025-09-16 18:15:53 -0500

    Versión inicial del proyecto catálogo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { IonicModule, AlertController } from '@ionic/angular';
+import { CommonModule } from '@angular/common';
+import { ProductCardComponent } from './components/product-card/product-card.component';
+import { PRODUCTS } from './data/products';
+import type { Product } from './models/product';
 
 @Component({
   selector: 'app-root',
-  templateUrl: 'app.component.html',
-  imports: [IonApp, IonRouterOutlet],
+  standalone: true,
+  imports: [IonicModule, CommonModule, ProductCardComponent],
+  templateUrl: './app.component.html'
 })
 export class AppComponent {
-  constructor() {}
+  products: Product[] = PRODUCTS;
+
+  constructor(private alertCtrl: AlertController) {}
+
+ async mostrarDetalle(product: Product) {
+  const alert = await this.alertCtrl.create({
+    header: product.name, // El título principal
+    subHeader: `Precio: $${product.price}`, // Un subtítulo para el precio
+    message: product.description ?? '', // El mensaje principal con solo la descripción
+    buttons: ['Cerrar']
+  });
+  await alert.present();
+}
 }
